0b87e9e1b1aca70fbc87a7e7a099e00fc0e684ad
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-20 08:56:26 -0600

    modify: Adding exceptionHandler for NotFoundException class

--- src/main/java/dev/gerardo/geolocationatm/apirest/exceptions/handlers/ExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package dev.gerardo.geolocationatm.apirest.exceptions.handlers;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ExceptionHandler {
-  
-}
