ab47ef18ed7f44f7921099b26df109452250d87c
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-20 08:45:30 -0600

    add: Adding ExceptionHandler class to exceptions.handler package

---
+++ src/main/java/dev/gerardo/geolocationatm/apirest/exceptions/handlers/ExceptionHandler.java
@@ -0,0 +1,8 @@
+package dev.gerardo.geolocationatm.apirest.exceptions.handlers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+  
+}
